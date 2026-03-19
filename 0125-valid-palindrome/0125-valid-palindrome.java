c8c43eb46726229ee22e96f83871bc14c1515f2e
Surendranathan K <krsurendra2007@gmail.com>
2026-03-19 20:53:58 +0530

    Time: 2 ms (99.22%), Space: 44.4 MB (73.3%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if (s == null) return false;
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        String s1 = "A man, a plan, a canal: Panama";
+        System.out.println("Input: " + s1);
+        System.out.println("Output: " + sol.isPalindrome(s1));
+
+        String s2 = "race a car";
+        System.out.println("Input: " + s2);
+        System.out.println("Output: " + sol.isPalindrome(s2));
+
+        String s3 = " ";
+        System.out.println("Input: \"" + s3 + "\"");
+        System.out.println("Output: " + sol.isPalindrome(s3)); 
+    }
+}
